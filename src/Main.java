737f7a826c876a289e1930b65346fc6ac335daff
621352 <camarenburt>
2023-12-18 09:35:08 -0500

    esgf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String one = "Celsius";
+        String two = "Fahrenheit";
+
+        System.out.printf("%10s   %10s",one, two);
+        for(double i = -100; i <= 100; i++ ){
+            System.out.printf("\n%10.2f   %10.2f", i,InputHelper.CtoF(i));
+        }
     }
 }
